96e57d24faf7debb168a967851222c2308f0a9e2
ersauravadhikari <ersauravadhikari@gmail.com>
2020-09-17 10:37:42 +0545

    Add Storage Files dir changed

--- SauravBenchmark/settings.py
+++ SauravBenchmark/settings.py
@@ -124,4 +124,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/opt/build/static')
