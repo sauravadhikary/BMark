f04845e035013842015b3ab645f66c21c3c73285
sauravadhikary <61972671+sauravadhikary@users.noreply.github.com>
2020-09-17 11:46:38 +0545

    U

--- SauravBenchmark/settings.py
+++ SauravBenchmark/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    '../../static',
+    os.path.join(BASE_DIR, '/static'),
 ]
 STATIC_ROOT ='../../static'
