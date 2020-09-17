201888700b28b966a6db8db570d383c7558dc8eb
ersauravadhikari <ersauravadhikari@gmail.com>
2020-09-17 12:23:31 +0545

    Removed Test

--- Database/views.py
+++ Database/views.py
@@ -5,12 +5,11 @@ from datetime import datetime
 def index(request):
     initialTime1 = datetime.now()
     test = "Benchmarking the Web Servers"
-    for i in range(10000):
-        print(test)
-        if len(test) < 200000000:
-            test += test
-        else:
-            test = test[0:len(test)// 2]  # if the string gets too long just cut it
+    # for i in range(10000):
+    #     if len(test) < 200000000:
+    #         test += test
+    #     else:
+    #         test = test[0:len(test)// 2]  # if the string gets too long just cut it
     finalTime1 = datetime.now()
     timeDiffrence1 = finalTime1 - initialTime1
     return HttpResponse(f"The Process took {timeDiffrence1}")
