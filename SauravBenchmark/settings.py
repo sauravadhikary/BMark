dac75acf6ae4277aa208819520e6c20b8065087f
ersauravadhikari <ersauravadhikari@gmail.com>
2020-09-17 12:40:45 +0545

    MEME ERROR Fixed

--- SauravBenchmark/settings.py
+++ SauravBenchmark/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    'static',
+    os.path.join(BASE_DIR, 'static/'),
+    'static/',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
